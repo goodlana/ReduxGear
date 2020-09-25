59a9a8b26d578ac08d69d13fefd2e227f4726d55
goodlana <lahee13579@gmail.com>
2020-09-25 16:47:03 +0900

    1-store-created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import cartItems from './cart-items'
+import NavBar from './NavBar';
+import CartContainer from './CartContainer';
+import { createStore } from 'redux';
+
+function reducer() {
+  console.log("shake and bake");
+}
+const store = createStore(reducer);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <main>
+      <NavBar />
+      <CartContainer cart={cartItems}/>
+    </main>
   );
 }
 
---
+++ src/CartContainer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import CartItem from './CartItem'
+
+
+const CartContainer = ({ cart }) => {
+  if(cart.length === 0) {
+    return (
+      <section className="cart">
+        {/* cart header */}
+        <header>
+          <h2>장바구니</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    )
+  }
+  return (
+    <section className="cart">
+      {/* cart header */}
+      <header>
+        <h2>장바구니</h2>
+      </header>
+      {/* cart items */}
+      <article>
+        {cart.map(item => {
+          return <CartItem key={item.id} {...item} />
+        })}
+      </article>
+      {/* cart footer */}
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>0.0만원</span>
+          </h4>
+        </div>
+        <button className="btn-clear">장바구니 비우기</button>
+      </footer>
+    </section>
+  )
+}
+
+export default CartContainer
